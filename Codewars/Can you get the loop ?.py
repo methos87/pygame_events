699bac2ffe5d0f4bfeb87816716284c46eb0553d
methos87 <novakakos87@gmail.com>
2022-10-03 12:40:03 +0200

    This is a commit

---
+++ Codewars/Can you get the loop ?.py
@@ -0,0 +1,53 @@
+#!usr/bin/env python
+
+from myLinkedList import LinkedList
+from myLinkedList import Node
+
+
+my_LinkedList = LinkedList()
+
+first_node = Node("a")
+my_LinkedList.head = first_node
+
+second_node = Node("b")
+third_node = Node("c")
+
+first_node.next = second_node
+second_node.next = third_node
+
+print(my_LinkedList)
+
+my_LinkedList.add_first(Node("k"))
+my_LinkedList.add_first(Node("v"))
+my_LinkedList.add_first(Node("e"))
+
+for node in my_LinkedList:
+    print(node)
+
+my_LinkedList.add_last(Node("y"))
+my_LinkedList.add_last(Node("i"))
+
+print(my_LinkedList)
+
+my_LinkedList.add_after("v", Node("22"))
+
+print(my_LinkedList)
+
+my_LinkedList.add_before("c", Node("ttt"))
+
+print(my_LinkedList)
+
+my_LinkedList.remove_node("ttt")
+
+print(my_LinkedList)
+
+my_LinkedList.get_len()
+my_LinkedList.get(1)
+my_LinkedList.get(9)
+
+my_LinkedList.reverse()
+
+print(my_LinkedList)
+
+
+
